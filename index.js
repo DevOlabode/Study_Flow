3554c3c47eb1397af6a6ce8db029b967a6bae14e
Samuel Olabode <Solabode499@gmail.com>
2025-11-28 21:42:11 -0700

    `moved index.html file to views directory`

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, "views")));
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+})
+
 const PORT = process.env.PORT;
 app.listen(PORT, ()=> console.log(`App is Listening on PORT ${PORT}`));
