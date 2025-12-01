550c6c6e934c7e5d80dc8b045ebca111a6c60f4c
Samuel Olabode <Solabode499@gmail.com>
2025-11-30 20:49:52 -0700

    `Refactored auth.js`

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express= require('express');
 const router = express.Router();
-const controller = require('../controllers/auth')
-router.get('/login', controller.loginForm)
+const controller = require('../controllers/auth');
+
+router.get('/login', controller.loginForm);
+
 module.exports = router;
