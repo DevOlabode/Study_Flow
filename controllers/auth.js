dda86a647af13a934fb858b4706bd10703d07987
Samuel Olabode <Solabode499@gmail.com>
2025-11-30 19:23:21 -0700

    `Update loginForm function to send login.html instead of test2.html`

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,5 +2,5 @@ const User = require('../models/user');
 const path = require('path')
 
 module.exports.loginForm = (req, res)=>{
-    res.sendFile(path.join(__dirname, "views", "test2.html"));
+    res.sendFile(path.join(__dirname, "../views", "auth/login.html"));
 };
