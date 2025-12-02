f9098ea716ca4c058c48ead9e83486fef75b7831
Samuel Olabode <Solabode499@gmail.com>
2025-12-02 04:20:45 -0700

    `Added login form submission and backend login handling`

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,3 +4,8 @@ const path = require('path')
 module.exports.loginForm = (req, res)=>{
     res.sendFile(path.join(__dirname, "../views", "auth/login.html"));
 };
+
+module.exports.login = async(req, res)=>{
+    const { email, password } = req.body;
+    console.log(req.body)
+}
