fb5819384feb0f3cba9985608b6c8b19995acb26
Samuel Olabode <Solabode499@gmail.com>
2025-12-03 04:21:56 -0700

    `Added express.urlencoded middleware and updated session configuration`

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const User = require('./models/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use(session(sessionConfig));
 app.use(flash())
 
